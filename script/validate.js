e714939b3daf9c548903a8956640185382f3c53c
Anna Revidovich <nutkatuz@yandex.ru>
2020-07-14 03:46:47 +0500

    add validation

--- script/validate.js
+++ script/validate.js
@@ -1,71 +1,76 @@
 const showInputError = (inputSelector, errorMessage, config) => {
-  const errorElement = inputSelector.closest(config.labelSelector).querySelector(config.redSpanSelector);
-  inputSelector.classList.add(config.inputErrorClass);
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add(config.errorClass);
+  const errorElement = inputSelector.closest(config.labelSelector).querySelector(config.redSpanSelector)
+  inputSelector.classList.add(config.inputErrorClass)
+  errorElement.textContent = errorMessage
+  errorElement.classList.add(config.errorClass)
 };
 
 const hideInputError = (inputSelector, config) => {
-  const errorElement = inputSelector.closest(config.labelSelector).querySelector(config.redSpanSelector);
-  inputSelector.classList.remove(config.inputErrorClass);
-  errorElement.classList.remove(config.errorClass);
-};
+  const errorElement = inputSelector.closest(config.labelSelector).querySelector(config.redSpanSelector)
+  inputSelector.classList.remove(config.inputErrorClass)
+  errorElement.classList.remove(config.errorClass)
+}
 
 const checkInputValidity = (inputSelector, config) => {
   if(!inputSelector.validity.valid) {
-      showInputError(inputSelector, inputSelector.validationMessage, config);
+      showInputError(inputSelector, inputSelector.validationMessage, config);//не применилось в ньюКард=ошибка
   } else {
-      hideInputError(inputSelector, config);
+      hideInputError(inputSelector, config)
   }
 };
 //живая валидация
 const setEventListeners = (formSelector, config) => {
-  const inputList = Array.from(formSelector.querySelectorAll(config.inputSelector));
-  const submitButtonSelector = formSelector.querySelector(config.submitButtonSelector);
-  
-  inputList.forEach((inputSelector) => {
+  const inputList = Array.from(formSelector.querySelectorAll(config.inputSelector))
+  const submitButtonSelector = formSelector.querySelector(config.submitButtonSelector)
+    inputList.forEach((inputSelector) => {
       inputSelector.addEventListener('input', () => {
-          // toggleButtonState(inputList, submitButtonSelector, config);
-          checkInputValidity(inputSelector, config);
+          toggleButtonState(inputList, submitButtonSelector, config)
+          checkInputValidity(inputSelector, config)
       });
   });
 };
 
 const firstStatePopup = (formSelector, config) => {
-  const inputList = Array.from(formSelector.querySelectorAll(config.inputSelector));
+  const inputList = Array.from(formSelector.querySelectorAll(config.inputSelector))
   inputList.forEach((inputSelector) => {//не показываем ошибки, не пугаем
-    hideInputError(inputSelector, config);
+    hideInputError(inputSelector, config)
   });
-  //но кнопка неактивна
   const submitButtonSelector = formSelector.querySelector(config.submitButtonSelector)
-  toggleButtonState(inputList, submitButtonSelector, config);
+  submitButtonSelector.classList.remove(config.inactiveButtonClass)
+  submitButtonSelector.removeAttribute('disabled')
 };
 //-----------------------------------------------------
 function enableValidation(config) {
-  const formElements = Array.from(document.querySelectorAll(config.formSelector));
+  const formElements = Array.from(document.querySelectorAll(config.formSelector))
   formElements.forEach((formSelector) => {
-    firstStatePopup(formSelector, config);
+    //сделать так чтоб убирался кр цвет и ошибка старая при тоггл
+    //firstStatePopup(formSelector, config)
+    ////ошибка - убирает спан только первого поля
+    // const inputSelector = somepopup.querySelector('.popup__input')
+    // hideInputError(inputSelector, config)
       formSelector.addEventListener('submit', (event) => {
-          event.preventDefault();
-      });
-      setEventListeners(formSelector, config);
-  });
-};
+          event.preventDefault()
+      })
+      setEventListeners(formSelector, config)
+  })
+}
 
 // кнопка отправки формы неактивна, если хотя бы одно из полей не проходит валидацию;
 function hasInvalidInput(inputList) {
   return inputList.some((inputSelector) => {
-      return !inputSelector.validity.valid;
-  });
-};
+      return !inputSelector.validity.valid
+  })
+}
+
 function toggleButtonState(inputList, submitButtonSelector, config) {
   if (hasInvalidInput(inputList)) {
-      submitButtonSelector.classList.add(config.inactiveButtonClass);
+      submitButtonSelector.classList.add(config.inactiveButtonClass)
+      submitButtonSelector.setAttribute('disabled', '')
   } else {
-      submitButtonSelector.classList.remove(config.inactiveButtonClass);
-      submitButtonSelector.removeAttribute('disabled');
+      submitButtonSelector.classList.remove(config.inactiveButtonClass)
+      submitButtonSelector.removeAttribute('disabled')
   }
-};
+}
 
 //config
 // enableValidation({
