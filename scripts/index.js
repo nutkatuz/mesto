3a131562dac295bd527eb139588e379e91a234aa
Anna Revidovich <nutkatuz@yandex.ru>
2020-07-16 21:32:47 +0500

    bugfix: comply with UX-design (disabled btn is always white according to Figma)
    add class card
    
    add class card
    
    add class card
    
    add class card
    
    add class card
    
    add class card
    
    add class
    
    add moduls
    
    bugfix: close zoom by esc
    
    refactoring
    
    refactoring
    
    add configCard for comf-use in other codes
    
    eliminate duplication
    
    eliminate duplication
    
    eliminate duplication

--- scripts/index.js
+++ scripts/index.js
@@ -1,142 +1,83 @@
-//для профиля
+import { openPopup, closePopup, popupProfile, popupNewCard } from './Popups.js'
+import { initialCards, configCard, config } from './utils.js'
+import { Card } from './Card.js'
+import { FormValidator } from './FormValidator.js'
+
+const addButton = document.querySelector('.profile__add-button')
+const cardsSection = document.querySelector('.places')
+
 const profileName = document.querySelector('.profile__name')
 const profileJob = document.querySelector('.profile__job')
 const editButton = document.querySelector('.profile__edit-button')
-//для окна редактирования
-const popupProfile = document.querySelector('.popup_profile-edit')
+
 const nameInput = document.querySelector('.popup__input_name')
 const jobInput = document.querySelector('.popup__input_about')
-const formPopupProfile = popupProfile.querySelector('.popup__window')
-const closeButtonPopupProfile = popupProfile.querySelector('.popup__close')
-//для карточки
-const addButton = document.querySelector('.profile__add-button')
-const cardsSection = document.querySelector('.places')
-const cardTemplate = document.querySelector('#card-template')
-//для окна добавления новой карточки
-const popupNewCard = document.querySelector('.popup_new-card')
+const profilePopupForm = popupProfile.querySelector('.popup__window')
+
 const placeInput = document.querySelector('.popup__input_place-name')
 const linkInput = document.querySelector('.popup__input_image_url')
-const formPopupNewCard = popupNewCard.querySelector('.popup__window')
-const closeButtonPopupNewCard = popupNewCard.querySelector('.popup__close')
-//для просмотра фото 
-const popupZoom = document.querySelector('.popup_zoom')
-const closeButtonPopupZoom = popupZoom.querySelector('.popup__close')
-const zoomImage = document.querySelector('.zoom__image')
-const zoomTitle = document.querySelector('.zoom__caption')
+const newCardPopupForm = popupNewCard.querySelector('.popup__window')
+const cardTemplateSelector = '.card-template'
 
-// toggle не подходит для этой работы. при закрытии окна с неправильными данными, точнее пока оно гаснет,
-// можно заметить, как кнопка "сохранить" меняет свое состояние с белой на черную. потом гаснет окончательно.
-// и еще когда быстро клацаешь по оверлею это же окно мигает, оно же не закрывается полностью,
-// у меня отсрочка, то есть когда окно гаснет, оно ещё открыто на странице, и тогл применяется на оверлее окна.
-// const togglePopup = function (somepopup) {
-//     somepopup.classList.toggle('popup_is-opened')
-//     resetFormState(somepopup, config)
-// } - убрала.
+const profileValidation = new FormValidator(config, profilePopupForm)
+const cardValidation = new FormValidator(config, newCardPopupForm)
 
-function openPopup(somepopup) {
-    somepopup.classList.add('popup_is-opened')
-    resetFormState(somepopup, config)
-    document.addEventListener('keyup', closePopupEsc)
+cardValidation.enableValidation()
+profileValidation.enableValidation()
+
+const cardsRender = function (item) {
+    const card = new Card(item, cardTemplateSelector, configCard)
+    const cardElement = card.generateCard()
+    return cardElement
 }
 
-function closePopup(somepopup) {
-    somepopup.classList.remove('popup_is-opened')
-    document.removeEventListener('keyup', closePopupEsc)
+const firstCardsSectionAutoFill = function () {
+    initialCards.forEach((item) => {
+    const cardElement = cardsRender(item)
+    cardsSection.append(cardElement)
+    })
+}
+firstCardsSectionAutoFill()
+
+const addNewCard = function () {
+    const name = placeInput.value
+    const link = linkInput.value
+    const cardElement = cardsRender({ name, link })
+    cardsSection.prepend(cardElement)
 }
 
-function closePopupEsc(event) {
-    const  KEYCODE_ESC = 27 //нет магических чисел
-        if (event.keyCode !== KEYCODE_ESC) {
-            return;
-        }
-    const openedPopup = document.querySelector('.popup_is-opened')
-    if (openedPopup) {
-        closePopup(openedPopup)
-    }
-}
-
-//понятно, на что именно следует нажать, чтобы модальное окно закрылось
-function overlayClose(event) {
-    if (event.target.classList.contains('popup_is-opened')){
-    const openedPopup = document.querySelector('.popup_is-opened')
-        if (openedPopup) {
-            closePopup(openedPopup)
-        }
-    }
-}
-
+//
 const showEditPopup = function () {
     openPopup(popupProfile)
     nameInput.value = profileName.textContent
     jobInput.value = profileJob.textContent
+    const submitButtonSelector = popupProfile.querySelector('.popup__button')
+    submitButtonSelector.classList.remove(config.inactiveButtonClass)
+    profileValidation.resetFormState()
 }
 
-const formSubmitHandlerProfile = function (event) {
-    event.preventDefault()
+const showNewCardPopup = function () {
+    openPopup(popupNewCard)
+    newCardPopupForm.reset()
+    const submitButtonSelector = popupNewCard.querySelector('.popup__button')
+    submitButtonSelector.classList.add(config.inactiveButtonClass)
+    cardValidation.resetFormState()
+}
+
+const formSubmitHandlerProfile = function (evt) {
+    evt.preventDefault()
     profileName.textContent = nameInput.value
     profileJob.textContent = jobInput.value
     closePopup(popupProfile)
 }
 
-const showNewCardPopup = function () {
-    openPopup(popupNewCard)
-    formPopupNewCard.reset()
-    resetFormState(popupNewCard, config)
-    //деактивирую кнопку "сохранить"
-    const submitButtonSelector = popupNewCard.querySelector('.popup__button')
-    submitButtonSelector.setAttribute('disabled', false)
-}
-
-const doLike = (evt) => evt.target.classList.toggle('card__like_active')
-
-function addCard(nameArg, linkArg) {
-    const card = cardTemplate.content.cloneNode(true)
-    const recycleBin = card.querySelector('.card__recycle-bin')
-    const likeBtn = card.querySelector('.card__like')
-    const cardTitle = card.querySelector('.card__title')
-    const cardImg = card.querySelector('.card__image')
-    cardTitle.textContent = nameArg
-    cardImg.src = linkArg
-    cardImg.setAttribute('alt', `Изображение ${nameArg}`)
-    cardImg.addEventListener('click', () => showZoomPopup(nameArg, linkArg))
-    recycleBin.addEventListener('click', (evt) => evt.target.closest('.card').remove())
-    likeBtn.addEventListener('click', doLike)
-    return card
-}
-
-function renderCard(item) {
-    const card = addCard(item.name, item.link)
-    cardsSection.prepend(card)
-}
-
-initialCards.forEach(function (item) {
-    addCard(item.name, item.link)
-    renderCard(item)
-})
-
-function formSubmitHandlerNewCard(evt) {
+const formSubmitHandlerNewCard = function (evt) {
     evt.preventDefault();
-    addCard(placeInput.value, linkInput.value)
-    const name = placeInput.value
-    const link = linkInput.value
-    renderCard({ name, link })
+    addNewCard(placeInput.value, linkInput.value)
     closePopup(popupNewCard)
-}
-
-function showZoomPopup(nameArgument, linkArgument) {
-    openPopup(popupZoom)
-    zoomTitle.textContent = nameArgument
-    zoomImage.setAttribute('src', `${linkArgument}`)
-    zoomImage.setAttribute('alt', `Изображение ${nameArgument}`)
 }
 
 editButton.addEventListener('click', showEditPopup)
 addButton.addEventListener('click', showNewCardPopup)
-formPopupProfile.addEventListener('submit', formSubmitHandlerProfile)
-popupZoom.addEventListener('mousedown', overlayClose)
-popupProfile.addEventListener('mousedown', overlayClose)
-popupNewCard.addEventListener('mousedown', overlayClose)
-formPopupNewCard.addEventListener('submit', formSubmitHandlerNewCard)
-closeButtonPopupProfile.addEventListener('click', () => closePopup(popupProfile))
-closeButtonPopupZoom.addEventListener('click', () => closePopup(popupZoom))
-closeButtonPopupNewCard.addEventListener('click', () => closePopup(popupNewCard))
+profilePopupForm.addEventListener('submit', formSubmitHandlerProfile)
+newCardPopupForm.addEventListener('submit', formSubmitHandlerNewCard)
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,26 +1,43 @@
-const initialCards = [
+export const initialCards = [
     {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Березники',
-        link: './images/places/zimniy.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+        name: 'Якутия',
+        link: './images/places/yakutiya.jpg'
     },
     {
         name: 'Мурманск',
         link: './images/places/murmanskbarincevo.jpg'
     },
     {
-        name: 'Якутия',
-        link: './images/places/yakutiya.jpg'
-    }
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    },
+    {
+        name: 'Березники',
+        link: './images/places/zimniy.jpg'
+    },
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    }, // висячая запятая
 ]
+
+export const configCard = {
+    zoomImageSelector: '.zoom__image',
+    zoomTitleSelector: '.zoom__caption'
+}
+
+export const config = { //объект настроек с селекторами и классами формы
+    formSelector: '.popup__window',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__button',
+    inactiveButtonClass: 'popup__button_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__error_visible',
+    redSpanSelector: '.popup__error',
+    labelSelector: '.popup__label',
+    fieldsetSelector: '.popup__content'
+}
